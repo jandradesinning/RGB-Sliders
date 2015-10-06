8058ba55a1a4899b1e25f003dc4f1f9dbceeb0ca
Jose F. Andrade-Sinning <jandradesinning@gmail.com>
2015-10-06 04:23:21 -0400

    added sliders and color square

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
+        
+        sliderMoved(colorSquare)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sliderMoved(sender: AnyObject) {
+        
+        colorSquare.backgroundColor = UIColor(red: CGFloat(redSlider.value), green:CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(1.0))
+        
+    }
 // added a comment
 }
 
